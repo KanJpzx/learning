6a80c641d95149057ced911685a8fa92c8239046
KanJpzx <haurtimes@gmail.com>
2026-08-29 21:42:11 +0200

    updated mod details?

--- src/main/java/com/kanjpz/meowski/meowski.java
+++ src/main/java/com/kanjpz/meowski/meowski.java
@@ -1,5 +1,7 @@
 package com.kanjpz.meowski;
 
+import com.kanjpz.meowski.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -31,6 +33,9 @@ public class meowski {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        //for items of some kind I don't even know
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -44,6 +49,9 @@ public class meowski {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.WILLOW_SAPLING);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
